8d2924b64e8bc2536abe8e269cefba2153cbd57d
wefcser <2432963477@qq.com>
2017-04-13 15:42:04 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+网页资源在/src/resources下，静态放在在static里，动态放在templtes里。
---
+++ src/main/java/com/GreetingController.java
@@ -0,0 +1,21 @@
+package com;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by wangyifei on 2017/4/13.
+ */
+@Controller
+public class GreetingController {
+
+    @RequestMapping(value = "/greeting",method = RequestMethod.GET)
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
