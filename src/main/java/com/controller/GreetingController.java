4873ba618bca20a969ce8834ca4d8cd958539d88
wefcser <2432963477@qq.com>
2017-04-20 15:12:58 +0800

    add DB

--- src/main/java/com/controller/GreetingController.java
+++ src/main/java/com/controller/GreetingController.java
@@ -1,11 +1,11 @@
-package com;
+package com.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * Created by wangyifei on 2017/4/13.
--- src/test/java/com/VdbserverApplicationTests.java
+++ src/test/java/com/VdbserverApplicationTests.java
@@ -1,16 +1,26 @@
 package com;
 
+import com.model.Vulnerability;
+import com.repostory.VulnerabilityRepostory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class VdbserverApplicationTests {
-
+	@Autowired
+	private VulnerabilityRepostory v;
 	@Test
 	public void contextLoads() {
+		v.save(new Vulnerability("wef"));
+		List<Vulnerability> t= v.findAll();
+		System.out.print(t.size()+"\n"+t.toString());
 	}
 
 }
